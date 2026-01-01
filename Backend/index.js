7388f9f17ad93d2ae2b274793506a4a3b6fc33b4
Sumant Bobade <167948550+SumantBobade@users.noreply.github.com>
2026-01-01 16:20:01 +0530

    Fix CORS origin URL by removing trailing slash

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://glow-fox.vercel.app/"],
+    origin: ["http://localhost:5173", "https://glow-fox.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
